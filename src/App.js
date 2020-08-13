8960663a3c9454900db1dd4ae195da0fce3f3e6d
Arkadiy <arkadiy.andriyenko@gmail.com>
2020-08-13 17:24:50 +0300

    added css normalize from Gardens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 //@flow
 import React from "react";
+import {
+  ThemeProvider,
+  DEFAULT_THEME,
+  PALETTE,
+} from "@zendeskgarden/react-theming";
+import { Dropdown, Menu, Item, Trigger } from "@zendeskgarden/react-dropdowns";
+
+import { Dots } from "@zendeskgarden/react-loaders";
 
 function App() {
-  return <div className="container">Hello World!</div>;
+  return (
+    <ThemeProvider theme={{ ...DEFAULT_THEME, rtl: true }}>
+      <h1>Hello</h1>
+      <Dropdown onSelect={(value) => console.log(`Selected: ${value}`)}>
+        <Trigger>
+          <button>This triggers a menu</button>
+        </Trigger>
+        <Dots color={PALETTE.blue[600]} />
+        <Menu placement="end" arrow>
+          <Item value="option-1">Option 1</Item>
+          <Item value="option-2">Option 2</Item>
+          <Item value="option-3">Option 3</Item>
+        </Menu>
+      </Dropdown>
+    </ThemeProvider>
+  );
 }
 
 export default App;
